19dcc8dfd4446fde1d457fe12ccad95b2f5c50de
DesmondM <desmondmpofu@gmail.com>
2021-11-28 04:54:47 +0200

    final

--- pages/api/vouchers.js
+++ pages/api/vouchers.js
@@ -4,40 +4,44 @@
   res.status(200).json({ Vouchers: vouchers })
 }
  
-
-const existCons = (str)=>{
-    var flag1 = false;
+const existConsecutives = (str)=>{
+    
     for(let i=0; i<str.length-2; i++){
         if(str[i]==str[i+1]&& str[i]==str[i+2]){
-            console.log("There are consecutive " + str[i] +"'s");
+          //  console.log("There are consecutive " + str[i] +"'s");
             return true
             break
         }
     }
     return false
 }
-console.log(existCons("122335676788755"));
 
-var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 var vouchers = []
 var charactersLength = characters.length;
 
-for (var j = 0; j<=400; j++){
-var result = ""
+var accepedVouchers=0
+var rejectedVouchers =0  // to roughly check the code's repetation rate
+
+while (accepedVouchers<40000){
+var voucher = ""
 for ( var i = 0; i < 10 ; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    voucher += characters.charAt(Math.floor(Math.random() * charactersLength));
 }
-if (existCons(result)){
-     console.log(result + " random number with consecutives ")
-
+if (existConsecutives(voucher)){
+     //console.log(voucher + " Bad Voucher ")
+     rejectedVouchers++;
 }else{
-    console.log(result + " I got the job ")
-    if(vouchers.indexOf(result)===-1){
-        vouchers.push(result);
-    }
+    //console.log(voucher + " Good voucher ")
+    if(vouchers.indexOf(voucher)===-1){
+        vouchers.push(voucher);
+        accepedVouchers++;
+    }else{rejectedVouchers++;}
 }
 
 }
 
+
+console.log("There are "+rejectedVouchers + " bad vouchers");
 console.log("There are "+vouchers.length + " vouchers");
 console.log(vouchers)
